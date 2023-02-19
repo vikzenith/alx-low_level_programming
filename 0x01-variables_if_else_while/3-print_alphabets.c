c6a7ba8b9d701cdaa6adaafaaefe8313a99dda10
vikzenith <akorvikkyy@gmail.com>
2023-02-18 23:50:12 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+       	char c;
+	
+	char d;
+	
+	c ='a';
+	d = 'A';
+	while
+		(c <= 'z'){
+			putchar(c);
+			c++;
+		}
+	while
+		(c <= 'Z'){
+			putchar(c);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
